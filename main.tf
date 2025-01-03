b5278f20e5c2ad76c654b7ce2b017bad5c90fbc9
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2025-01-02 18:19:33 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region
+  region = "us-east-1"  # Replace with your desired AWS region 
 }
 
 resource "aws_vpc" "main_vpc" {
